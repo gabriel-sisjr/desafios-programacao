139f2571da1576f5642f2f7b32954b3cd30f088b
rodrigocesarsalmeida <rodrigocesarsalmeida@hotmail.com>
2020-10-01 23:49:32 -0300

    Desafio AspNEt-MVC Rodrigo Cesar

---
+++ Projetos/ASPNET-MVC/Rodrigo Cesar/APIPROTECH/APIPROTECH/models/Experiencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPROTECH.Models
+{
+    public class Experiencia
+    {
+        public int Codigo { get; set; }
+        public string Tecnologia { get; set; }
+        public int TempoExperiencia { get; set; }
+        public string DetalheExperiencia { get; set; }
+        public Pessoa Pessoa { get; set; }
+
+    }
+}
---
+++ Projetos/ASPNET-MVC/Rodrigo Cesar/APIPROTECH/APIPROTECH/models/ExperienciaEmpresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPROTECH.Models
+{
+    public class ExperienciaEmpresa
+    {
+        public int Codigo { get; set; }
+        public string Empresa { get; set; }
+        public string Cargo { get; set; }
+        public string DataInicio { get; set; }
+        public string DataFim { get; set; }
+        public string DetalheExperiencia { get; set; }
+        public Pessoa Pessoa { get; set; }
+
+    }
+}
